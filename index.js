d6bd0d5a2b3e1d983b17efcade37d897a23b387a
dldushyanthgowda-hue <dldushyanthgowda@gmail.com>
2026-09-09 20:29:22 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -13,5 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-module.exports = require('playwright-core');
+require('./lib/bootstrap');
+module.exports = require('./lib/coreBundle').inprocess.playwright;
